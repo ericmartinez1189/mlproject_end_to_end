f777d4dfe2fb5170de58124e0314542072150edf
Eric Martínez <ericmartinez89@gmail.com>
2024-03-05 15:17:06 +0100

    docker file and docker yaml

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 import numpy as np 
 import pandas as pd
-from sklearn.compose import ColumnTransformer
+from sklearn import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
